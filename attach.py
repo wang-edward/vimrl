9f7bfc9997423f5cfe7ee2aa55b04481920c5786
Edward Wang <edawordwang@gmail.com>
2025-01-10 15:48:26 -0500

    basic script to test nvim can attach

---
+++ attach.py
@@ -0,0 +1,12 @@
+import pynvim
+nvim = pynvim.attach(
+    'child',
+    argv=[
+        "nvim",
+        "--embed",
+        "--headless",
+        "--clean",  # or '-u', 'NONE'
+    ],
+)
+
+print(nvim.funcs.mode())
